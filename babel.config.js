51754f623cbc0e113b2287ac284b324dc9b1468b
Kaustubh Karkare <kaustubh.karkare@gmail.com>
2020-06-02 20:28:46 -0700

    Use babel.config.js instead of .babelrc

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "plugins": [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-runtime"
+  ],
+  "presets": [
+    "@babel/preset-env",
+    "@babel/preset-react"
+  ],
+};
