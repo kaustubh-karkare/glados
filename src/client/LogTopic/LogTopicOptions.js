00484461b671580565278941eee00a8975d065a5
Kaustubh Karkare <kaustubh.karkare@gmail.com>
2022-07-16 13:58:40 -0700

    LogTopicOptions loads complete parentTopic

--- src/client/LogTopic/LogTopicOptions.js
+++ src/client/LogTopic/LogTopicOptions.js
@@ -8,22 +8,23 @@ const CREATE_ITEM = {
     __type__: 'log-topic',
     __id__: getVirtualID(),
     name: 'Create New Topic ...',
-    getItem(_option, parentLogTopic) {
-        return new Promise((resolve) => {
-            Coordinator.invoke('modal-editor', {
-                dataType: 'log-topic',
-                EditorComponent: LogTopicEditor,
-                valueKey: 'logTopic',
-                value: LogTopic.createVirtual({ parentLogTopic }),
-                onClose: (newLogTopic) => {
-                    if (newLogTopic && isRealItem(newLogTopic)) {
-                        resolve(newLogTopic);
-                    } else {
-                        resolve(null);
-                    }
-                },
-            });
-        });
+    getItem(_option, partialParentLogTopic) {
+        return window.api.send('log-topic-load', partialParentLogTopic)
+            .then((parentLogTopic) => new Promise((resolve) => {
+                Coordinator.invoke('modal-editor', {
+                    dataType: 'log-topic',
+                    EditorComponent: LogTopicEditor,
+                    valueKey: 'logTopic',
+                    value: LogTopic.createVirtual({ parentLogTopic }),
+                    onClose: (newLogTopic) => {
+                        if (newLogTopic && isRealItem(newLogTopic)) {
+                            resolve(newLogTopic);
+                        } else {
+                            resolve(null);
+                        }
+                    },
+                });
+            }));
     },
 };
 
